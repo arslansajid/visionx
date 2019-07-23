6d34802825d71d8cddce7ee210325e3dc0111f62
arslansajid <arslansajid951@gmail.com>
2019-07-23 12:36:56 +0500

    added routes to main router

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './containers/home';
 import Detail from './containers/detail';
+import FullResults from './containers/fullResults'
 
 
 const MainRouter = () => (
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route exact path="/detail" component={Detail} />
+    <Route exact path="/detail/:bookId" component={Detail} />
+    <Route exact path="/full-results" component={FullResults} />
   </Switch>
 )
 
