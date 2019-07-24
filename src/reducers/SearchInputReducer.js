47f0009053f52ca8cf1018f3e636cfc38a9c02b6
arslansajid <arslansajid951@gmail.com>
2019-07-24 12:00:26 +0500

    implemented search Input reducer

--- src/reducers/SearchInputReducer.js
+++ src/reducers/SearchInputReducer.js
@@ -1,13 +1,47 @@
-import { SET_SEARCH_INPUT, RESET_STORE } from '../actions/ActionTypes';
+import { SET_SEARCH_INPUT, RESULTS_FETCHED, INFINITE_SCROLL_RESULTS_FETCHED, REVIEWS_FETCHED } from '../actions/ActionTypes';
+import {combineReducers} from 'redux';
 
-export default function totalRoomCount(state = '', action) {
+export function searchedInput(state = '', action) {
     switch (action.type) {
       case SET_SEARCH_INPUT:
         console.log('SET_SEARCH_INPUT Action')
         return action.payload;
-      case RESET_STORE:
-        return '';
       default:
         return state;
     }
   }
+
+export function results(state = [], action) {
+  switch(action.type) {
+    case RESULTS_FETCHED:
+    return action.payload;
+    default: 
+    return state;
+  }
+}
+
+export function reviews(state = [], action) {
+  switch(action.type) {
+    case REVIEWS_FETCHED:
+    return action.payload;
+    default: 
+    return state;
+  }
+}
+
+export function inifiniteScrollResults(state = [], action) {
+  switch(action.type) {
+    case INFINITE_SCROLL_RESULTS_FETCHED:
+    return [ ...state, ...action.payload.searchResult ];
+    default: 
+    return state;
+  }
+
+}
+
+export default combineReducers({
+  searchedInput,
+  results,
+  reviews,
+  inifiniteScrollResults
+})
