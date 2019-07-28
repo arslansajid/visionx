8f6620f10283eb9f49b49f4342d0e06a2046daa7
arslansajid <arslansajid951@gmail.com>
2019-07-28 08:55:39 +0500

    Updates

---
+++ precache-manifest.2f66aa8085212b012c38acf71fd9ed14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4225012dc03e3bb160d54c82e42fea2f",
+    "url": "/visionx/index.html"
+  },
+  {
+    "revision": "be4c9ed16cc6c23304d2",
+    "url": "/visionx/static/css/2.38281306.chunk.css"
+  },
+  {
+    "revision": "1992f32f5fa209ffd952",
+    "url": "/visionx/static/css/main.3b59c189.chunk.css"
+  },
+  {
+    "revision": "be4c9ed16cc6c23304d2",
+    "url": "/visionx/static/js/2.daf1000d.chunk.js"
+  },
+  {
+    "revision": "1992f32f5fa209ffd952",
+    "url": "/visionx/static/js/main.8114aece.chunk.js"
+  },
+  {
+    "revision": "2951ca6227c5635dd8d6",
+    "url": "/visionx/static/js/runtime~main.18a40293.js"
+  },
+  {
+    "revision": "9eeccb7a60036e4b46f1ff99161cf7a1",
+    "url": "/visionx/static/media/library.9eeccb7a.jpg"
+  }
+]);
